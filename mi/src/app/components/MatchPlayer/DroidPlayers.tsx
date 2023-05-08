3f66b309441d765fe705d7eb4a32caf74c39e25c
Gerard del Castillo <gerard.castillo@capchase.com>
2023-05-08 23:03:31 +0200

    feat: add model

---
+++ mi/src/app/components/MatchPlayer/DroidPlayers.tsx
@@ -0,0 +1,86 @@
+import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
+import { EntityKind, Match } from "./types";
+import { useRef } from "react";
+import { TICK_RATE_MS } from "./constants";
+import droidUrl from "@/app/assets/DROID.glb?url";
+import { useGLTF } from "@react-three/drei";
+
+const tempPlayers = new THREE.Object3D();
+const tempColor = new THREE.Color();
+
+type Props = {
+  match: Match;
+};
+
+const DroidPlayers = ({ match }: Props) => {
+  const { nodes, materials } = useGLTF(droidUrl) as any;
+  const mesh6 = useRef<THREE.InstancedMesh>(null);
+  const mesh5 = useRef<THREE.InstancedMesh>(null);
+  const mesh4 = useRef<THREE.InstancedMesh>(null);
+  const body = useRef<THREE.InstancedMesh>(null);
+  const metal = useRef<THREE.InstancedMesh>(null);
+  const color = useRef<THREE.InstancedMesh>(null);
+  const meshes = [mesh6, mesh5, mesh4, body, metal, color];
+  const mapOffset = new THREE.Vector3(
+    -match.map.size[0] / 2,
+    0,
+    -match.map.size[1] / 2
+  );
+
+  useFrame(({ clock }) => {
+    const currentTick = Math.round(
+      (clock.elapsedTime * TICK_RATE_MS) % match.ticks.length
+    );
+    for (const entity of match.ticks[currentTick].entities) {
+      if (entity.kind !== EntityKind.PLAYER) continue;
+      if (entity.health <= 0) continue;
+      const [x, y] = entity.pos;
+      tempPlayers.position.set(x, 0, y);
+      tempPlayers.position.add(mapOffset);
+      tempPlayers.rotation.set(0, Math.sin(currentTick / 10), 0);
+      tempColor.setRGB(Math.random(), Math.random(), Math.random());
+      tempPlayers.updateMatrix();
+      for (const mesh of meshes) {
+        if (!mesh.current) continue;
+        mesh.current.setMatrixAt(entity.id, tempPlayers.matrix);
+        mesh.current.instanceMatrix.needsUpdate = true;
+      }
+      metal.current?.setColorAt(entity.id, tempColor);
+    }
+    // ref.current.instanceMatrix.needsUpdate = true;
+  });
+
+  return (
+    <group>
+      <instancedMesh
+        ref={color}
+        args={[nodes.Roundcube003.geometry, materials.COLOR, 200]}
+      />
+      <instancedMesh
+        ref={metal}
+        args={[nodes.Roundcube003_1.geometry, materials.metal, 200]}
+      />
+      <instancedMesh
+        ref={body}
+        args={[nodes.Roundcube003_2.geometry, materials.plastic, 200]}
+      />
+      <instancedMesh
+        ref={mesh4}
+        args={[nodes.Roundcube003_3.geometry, materials.GLOE, 200]}
+      />
+      <instancedMesh
+        ref={mesh5}
+        args={[nodes.Roundcube003_4.geometry, materials.logo, 200]}
+      />
+      <instancedMesh
+        ref={mesh6}
+        args={[nodes.Roundcube003_5.geometry, materials.LOGO, 200]}
+      />
+    </group>
+  );
+};
+
+export { DroidPlayers };
+
+useGLTF.preload(droidUrl);
--- mi/src/app/components/MatchPlayer/index.tsx
+++ mi/src/app/components/MatchPlayer/index.tsx
@@ -6,18 +6,27 @@ import domeVS from "@/app/shaders/dome.vs";
 import domeFS from "@/app/shaders/dome.fs";
 
 import { Match } from "./types";
-import { Players } from "./Players";
 import { Bullets } from "./Bullets";
+import { DroidPlayers } from "./DroidPlayers";
 
 type MatchSceneProps = {
   match: Match;
 };
+// hex value from vec3(5.0 / 255.0, 10.0 / 255.0, 25.0 / 255.0);
+const skyColor = 0x050a19;
+// hex value from vec3(10.0 / 255.0, 90.0 / 255.0, 100.0 / 255.0);
+const groundColor = 0x045460;
 
 const MatchScene = ({ match }: MatchSceneProps) => {
   return (
     <>
       <PerspectiveCamera makeDefault far={2000} />
-      <OrbitControls target={[0, 1.75, 0]} />
+      <OrbitControls
+        target={[0, 1.75, 0]}
+        maxDistance={match.map.size[0] * 1.5}
+        maxAzimuthAngle={Math.PI / 2}
+        maxPolarAngle={Math.PI / 2}
+      />
       <mesh>
         <sphereGeometry args={[match.map.size[0] * 2, 32, 32]} />
         <shaderMaterial
@@ -27,6 +36,8 @@ const MatchScene = ({ match }: MatchSceneProps) => {
           side={THREE.DoubleSide}
         />
       </mesh>
+      <ambientLight intensity={3} />
+      <hemisphereLight args={[skyColor, groundColor, 100]} />
       <Grid
         cellColor="#666"
         sectionColor="#444"
@@ -35,10 +46,9 @@ const MatchScene = ({ match }: MatchSceneProps) => {
         args={match.map.size}
         fadeDistance={800}
       />
-      <hemisphereLight intensity={0.8} args={["#fff", "#000", 1]} />
       <group position={[0, 1.75, 0]}>
         <Bullets match={match} />
-        <Players match={match} />
+        <DroidPlayers match={match} />
       </group>
     </>
   );
