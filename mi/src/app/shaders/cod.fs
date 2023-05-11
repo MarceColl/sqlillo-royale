902afde75f9235e3ea5e8114cef8646b51afbbdf
Gerard del Castillo <gerard.castillo@capchase.com>
2023-05-11 14:04:42 +0200

    fix: shader in cod

--- mi/src/app/shaders/cod.fs
+++ mi/src/app/shaders/cod.fs
@@ -12,10 +12,18 @@ float smoothness = 30.0;
 float offset = 0.0;
 
 
+//  Function from Iñigo Quiles
+//  www.iquilezles.org/www/articles/functions/functions.htm
+float expStep( float x, float k, float n ){
+    return exp( -k*pow(x,n) );
+}
+
 void main() {
-    float visible = float(pos.y > 0.0);
+    float aboveY = float(pos.y > 0.0);
     float mappedPos = (pos.y - offset)/smoothness;
-    float q = clamp(mappedPos, 0.0, 1.0);
+    float y = expStep(mappedPos,5.,1.0);
+    
+    float q = clamp(y, 0.0, 1.0);
     // gl_FragColor = vec4(q, q, q, 1.0);
-    gl_FragColor = mix(vec4(color, visible), vec4(color, 0.0), q);
+    gl_FragColor = mix(vec4(color, 0.0), vec4(color, aboveY), q);
 }
