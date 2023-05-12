fb19518444118044ec46ac43ec7488a3251a1645
Marcelino Coll <marce@dziban.net>
2023-05-12 22:57:20 +0200

    jas;dlkfj

--- mmz/main.c
+++ mmz/main.c
@@ -685,6 +685,7 @@ void *player_thread(void *data) {
 
   if (luaL_loadstring(L, code) || lua_pcall(L, 0, 0, 0)) {
     printf("[WARN] Player %d cannot run file: %s\n", id, lua_tostring(L, -1));
+    ptd->dead = true;
     return NULL;
   }
 
@@ -694,7 +695,6 @@ void *player_thread(void *data) {
   printf("[INFO] Player #%d called `bot_init` OK!\n", id);
 
   while (true) {
-    printf("PLAYER #%d PRELOCK\n", id);
     pthread_mutex_lock(&inc_tick_cond_mut);
     while (ptd->curr_tick == tick) {
       pthread_cond_wait(&inc_tick_cond_var, &inc_tick_cond_mut);
