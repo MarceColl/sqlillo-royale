9d07ca20154695f69a56372c98969f4a1156b104
Marcelino Coll <marce@dziban.net>
2023-05-12 23:59:03 +0200

    s;dlfkj;flkj

--- mmz/build.sh
+++ mmz/build.sh
@@ -1,5 +1,6 @@
 gcc -o mmz-bin *.c \
     -O2 -Wall -Wextra -Wpedantic \
+    -fsanitize=address -g \
     $(pkg-config --cflags --libs luajit) \
     $(pkg-config --cflags --libs libpq) \
     $(pkg-config --cflags --libs lua) \
